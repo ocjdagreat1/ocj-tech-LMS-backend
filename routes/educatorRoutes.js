a713ea53377ead659f48f4ea1a1d8c20d99dd664
ocjdagreat1 <oparachibuikejustine@gmail.com>
2026-02-16 22:42:19 -0800

    fix educator controller

--- routes/educatorRoutes.js
+++ routes/educatorRoutes.js
@@ -1,33 +1,21 @@
 import express from 'express'
-import {
-  addCourse,
-  educatorDashboardData,
-  getEducatorCourses,
-  getEnrolledStudentsData,
-  updateRoleToEducator
-} from '../controllers/educatorController.js'
-
+import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorController.js'
 import { requireAuth } from '@clerk/express'
 import upload from '../configs/multer.js'
 import { protectEducator } from '../middlewares/authMiddleware.js'
 
 const educatorRouter = express.Router()
 
-// Become educator (WRITE → POST)
-educatorRouter.post('/update-role', requireAuth(), updateRoleToEducator)
+// Become educator (only login required)
+educatorRouter.post('/update-role', requireAuth(),updateRoleToEducator)
 
-// Add course
-educatorRouter.post(
-  '/add-course',
-  requireAuth(),
-  protectEducator,
-  upload.single('image'),
-  addCourse
+// Real educator actions (login + role required)
+educatorRouter.post('/add-course',requireAuth(),protectEducator,upload.single('image'),
+addCourse
 )
 
-// Educator routes
-educatorRouter.get('/courses', requireAuth(), protectEducator, getEducatorCourses)
-educatorRouter.get('/dashboard', requireAuth(), protectEducator, educatorDashboardData)
-educatorRouter.get('/enrolled-students', requireAuth(), protectEducator, getEnrolledStudentsData)
+educatorRouter.get('/courses',requireAuth(),protectEducator, getEducatorCourses)
 
-export default educatorRouter;
+educatorRouter.get('/dashboard', requireAuth(),protectEducator,educatorDashboardData)
+educatorRouter.get('/enrolled-students',requireAuth(),protectEducator,getEnrolledStudentsData
+)
